96dc809a1ddfe6eeb84f183a95b6d5c7b72eafed
prahlad-web <prahlad.web@gmail.com>
2025-12-01 11:21:55 -0800

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,19 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,17 +22,42 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
+
   render() {
     return (
-      <Html lang='en-GB'>
+      <Html lang="en-GB">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          {/* Fonts */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+
+          {/* RevPie Tracking Script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var _clcktt = _clcktt || {};
+                _clcktt['cId'] = "117499";
+                document.write('<div id="clcktt"></div>');
+                (function() {
+                  var ca = document.createElement('script'); 
+                  ca.type = 'text/javascript';
+                  ca.async = true;
+                  ca.src = '//revpie.com/api/js/extractor.js';
+                  var sc = document.getElementsByTagName('script')[0]; 
+                  sc.parentNode.insertBefore(ca, sc);
+                })();
+              `,
+            }}
+          />
         </Head>
+
         <body>
           <Main />
           <NextScript />
